51be686cb7020876e320879fedf5e4e71cc3b93d
Артем Полтавський <54792047+artemexistent@users.noreply.github.com>
2020-11-08 11:20:58 +0200

    Introducing Spring (no database added)

---
+++ Term_1/Lab_3/src/main/java/com/example/servingwebcontent/repos/MessageRepo.java
@@ -0,0 +1,8 @@
+package com.example.servingwebcontent.repos;
+
+import com.example.servingwebcontent.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+}
