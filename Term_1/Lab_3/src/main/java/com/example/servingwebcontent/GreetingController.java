ab9e1152d1de41352472ddc4b7b4d0e55a04c8e8
Артем Полтавський <54792047+artemexistent@users.noreply.github.com>
2020-11-09 09:55:33 +0200

    Add Filter

--- Term_1/Lab_3/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ Term_1/Lab_3/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -35,6 +36,19 @@ public class GreetingController {
         Message message = new Message(name, email);
         Iterable<Message> messages = messageRepo.findAll();
         messageRepo.save(message);
+        model.put("messages",messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter,  Map<String, Object> model){
+        Iterable<Message> messages;
+
+        if (filter!=null && !filter.isEmpty()) {
+            messages = messageRepo.findByName(filter);
+        }else {
+            messages = messageRepo.findAll();
+        }
         model.put("messages",messages);
         return "main";
     }
--- Term_1/Lab_3/src/main/java/com/example/servingwebcontent/repos/MessageRepo.java
+++ Term_1/Lab_3/src/main/java/com/example/servingwebcontent/repos/MessageRepo.java
@@ -3,6 +3,10 @@ package com.example.servingwebcontent.repos;
 import com.example.servingwebcontent.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByName(String name);
 
 }
