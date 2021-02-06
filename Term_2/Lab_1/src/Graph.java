2471132ca677b26d0745c379ec60090d4703f965
Артем Полтавський <54792047+artemexistent@users.noreply.github.com>
2021-02-06 16:17:07 +0200

    Creature Graph

--- Term_2/Lab_1/src/Graph.java
+++ Term_2/Lab_1/src/Graph.java
@@ -1,16 +1,42 @@
 import java.util.Vector;
 
-public interface Graph {
-
+interface Graph {
+    void create(int n);
+    void insertRib(int x, int y, int z);
 }
 
 class MatrixGraph implements Graph {
     int matrix[][];
+
+    @Override
+    public void create(int n) {
+        matrix = new int[n][n];
+    }
+
+    @Override
+    public void insertRib(int x, int y, int z) {
+        matrix[x][y] = z;
+        matrix[y][x] = z;
+    }
 }
 
 
 class ListGraph implements Graph {
     Vector<Vector<int[]>> list;
+
+    @Override
+    public void create(int n) {
+        list = new Vector<>(0);
+        for (int i = 0; i < n; i++) {
+            list.add(new Vector<>(0));
+        }
+    }
+
+    @Override
+    public void insertRib(int x, int y, int z) {
+        list.get(x).add(new int[]{y, z});
+        list.get(y).add(new int[]{x, z});
+    }
 }
 
 class Fabric {
--- Term_2/Lab_1/src/Main.java
+++ Term_2/Lab_1/src/Main.java
@@ -7,7 +7,18 @@ public class Main {
         System.out.print("1. Matrix\n" + "2. List\n" + "Enter type graph: ");
         Graphs type = (scanner.nextInt() == 1 ? Graphs.MATRIX : Graphs.LIST);
         Graph graph = Fabric.createGraph(type);
-
+        System.out.print("Enter number of vertices:");
+        int n = scanner.nextInt();
+        graph.create(n);
+        System.out.print("Enter number of ribs:");
+        int m = scanner.nextInt();
+        System.out.printf("Enter %d ribs:\n", m);
+        for (int i = 0; i < m; i++) {
+            int x = scanner.nextInt() - 1;
+            int y = scanner.nextInt() - 1;
+            int z = scanner.nextInt();
+            graph.insertRib(x, y, z);
+        }
 
     }
 
