d4075b5277a69a60cff5510b6f6768034d4350cd
artempoltavskij <tanja7878andartem1616@gmail.com>
2021-04-12 15:53:35 +0300

    Created Ship

---
+++ Term_2/Lab_3/src/Ship.java
@@ -0,0 +1,37 @@
+public class Ship {
+    private int count;
+    private int size;
+    private Type type;
+
+    public Ship(int size, Type type) {
+        this.size = size;
+        this.type = type;
+    }
+
+    public void add(int count) {
+        this.count += count;
+    }
+
+    public boolean countCheck() {
+        if (count >= size) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
+enum Type {
+    BANANAS, SANDS, MELONS, STONES, GARBAGE;
+}
